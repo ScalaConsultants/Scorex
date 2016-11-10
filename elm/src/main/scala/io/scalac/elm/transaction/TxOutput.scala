32f71176238723c14cea0251bfc96a1290e72d9f
Adam Smolarek <adam.smolarek@angieslist.com>
2016-11-10 16:11:49 +0100

    fix for incoming transaction not modifying wallet balance

--- elm/src/main/scala/io/scalac/elm/transaction/TxOutput.scala
+++ elm/src/main/scala/io/scalac/elm/transaction/TxOutput.scala
@@ -7,10 +7,8 @@ import io.scalac.elm.serialization.ByteSerialization
 import scorex.core.transaction.box.Box
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 
-case class TxOutput(value: Long, proposition: PublicKey25519Proposition)
+case class TxOutput(value: Long, proposition: PublicKey25519Proposition, id: Array[Byte] = UUID.randomUUID().toString.getBytes(StandardCharsets.US_ASCII))
   extends Box[PublicKey25519Proposition] with ByteSerialization[TxOutput] {
-
-  val id = UUID.randomUUID().toString.getBytes(StandardCharsets.US_ASCII)
 
   def bytes: Array[Byte] = bytes(this)
 }
