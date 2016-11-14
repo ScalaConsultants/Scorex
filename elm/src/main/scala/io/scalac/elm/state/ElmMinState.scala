05c2e090a8d991895a94d3fc6539499b17935d4b
Adam Smolarek <adam.smolarek@angieslist.com>
2016-11-14 16:23:00 +0100

    restore lazy vals

--- elm/src/main/scala/io/scalac/elm/state/ElmMinState.scala
+++ elm/src/main/scala/io/scalac/elm/state/ElmMinState.scala
@@ -51,9 +51,9 @@ case class ElmMinState(storage: Map[ByteKey, TxOutput] = Map())
     val outputSum = tx.outputs.map(_.value).sum + tx.fee
     val addsUp = inputSum == outputSum
 
-    val positiveOuts = tx.outputs.forall(_.value > 0)
+    lazy val positiveOuts = tx.outputs.forall(_.value > 0)
 
-    val signed = tx.inputs.forall { in =>
+    lazy val signed = tx.inputs.forall { in =>
       val out = storage.get(in.closedBoxId)
       out.map(o => Curve25519.verify(in.boxKey.signature, o.bytes, o.proposition.pubKeyBytes)).exists(identity)
     }
